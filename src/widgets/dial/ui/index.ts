e13bcc8070b1d87389ec5f5613cd7d3b7a7ccc4c
Bori-github <qhflrnfl4324@gmail.com>
2026-08-20 02:29:59 +0900

    refactor: 슬라이스 중첩 배럴 제거

--- src/widgets/dial/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { DialArc } from './DialArc';
-export { DialHandle } from './DialHandle';
-export { DialItems } from './DialItems';
-export { DialReadout } from './DialReadout';
-export { TickNumbers } from './TickNumbers';
