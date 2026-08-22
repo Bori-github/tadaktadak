19848de3f0d4a1247052cac268d01c6a114514c8
Bori-github <qhflrnfl4324@gmail.com>
2026-08-23 03:46:28 +0900

    test: 렌더 테스트를 RNTL로 이관

--- src/shared/ui/dot-sprite/DotSprite.test.tsx
+++ src/shared/ui/dot-sprite/DotSprite.test.tsx
@@ -1,22 +1,19 @@
 import { describe, expect, it } from '@jest/globals';
-import { Canvas, Rect } from '@shopify/react-native-skia';
-import TestRenderer from 'react-test-renderer';
+import { Canvas } from '@shopify/react-native-skia';
+import { render, screen } from '@testing-library/react-native';
 
 import { DotSprite } from './DotSprite';
 
 describe('DotSprite', () => {
-  it('켜진 도트만 격자 좌표에 놓는다', () => {
+  it('켜진 도트만 격자 좌표에 놓는다', async () => {
     // Canvas 밖에서는 Skia 노드가 만들어지지 않아 감싼다
-    let tree!: TestRenderer.ReactTestRenderer;
-    TestRenderer.act(() => {
-      tree = TestRenderer.create(
-        <Canvas style={{ width: 200, height: 200 }}>
-          <DotSprite grid={['#.', '.#']} centerX={100} centerY={100} dotSize={4} colors={{ '#': '#ffffff' }} />
-        </Canvas>,
-      );
-    });
+    await render(
+      <Canvas style={{ width: 200, height: 200 }}>
+        <DotSprite grid={['#.', '.#']} centerX={100} centerY={100} dotSize={4} colors={{ '#': '#ffffff' }} />
+      </Canvas>,
+    );
 
-    const rects = tree.root.findAllByType(Rect).map(({ props }) => ({ x: props.x, y: props.y }));
+    const rects = screen.container.queryAll((node) => node.type === 'skRect').map(({ props }) => ({ x: props.x, y: props.y }));
 
     expect(rects).toEqual([
       { x: 96, y: 96 },
