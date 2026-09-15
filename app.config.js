da32b7a8948fdfdcae139177328c064af4c9290d
Bori-github <85009583+Bori-github@users.noreply.github.com>
2026-09-15 14:46:42 +0900

    ci: 릴리즈 PR의 Prettier 실패 수정
    Release Please가 동작하면서 app.json의 서식이 함께 변경되어 Prettier
    검사가 실패한다. 버전 관리를 package.json으로 모아 app.json을 대상에서
    제외하고, 생성물인 CHANGELOG.md는 서식 검사를 건너뛴다.

--- app.config.js
+++ app.config.js
@@ -1,5 +1,7 @@
 module.exports = ({ config }) => ({
   ...config,
+  // Release Please가 app.json을 수정하면서 서식이 함께 변경됨. 버전은 package.json에서 읽도록 처리
+  version: require('./package.json').version,
   ios: {
     ...config.ios,
     appleTeamId: process.env.EXPO_APPLE_TEAM_ID,
