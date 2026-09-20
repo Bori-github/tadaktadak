25f8d97106af700ee75f7ed757268eee622055e4
Bori-github <85009583+Bori-github@users.noreply.github.com>
2026-09-21 01:02:18 +0900

    test: jest 목의 Skia SVG 파싱을 스텁으로 교체
    DOMParser를 채워도 같은 함수가 이어서 Image와 document.body를 참조해 폴리필로는 끝나지 않는다. 메서드 하나만 교체한다.

--- jest.setup.js
+++ jest.setup.js
@@ -2,3 +2,7 @@ const { jest } = require('@jest/globals');
 
 // AsyncStorage는 네이티브 모듈을 찾지 못하면 import 시점에 던짐. 패키지가 함께 내는 테스트용 구현으로 바꿈
 jest.mock('@react-native-async-storage/async-storage', () => require('@react-native-async-storage/async-storage/jest/async-storage-mock'));
+
+// Skia의 jest 목에서 MakeFromString이 DOMParser를 참조해 ReferenceError. null 반환 스텁으로 교체
+const skipSvgParsing = () => null;
+require('@shopify/react-native-skia').Skia.SVG.MakeFromString = skipSvgParsing;
